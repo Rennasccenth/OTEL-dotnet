5526f91acffd1fa102042d023b67b84d2159d96f
Felipe Nunes <nullnes.sh@gmail.com>
2024-07-29 00:30:10 -0300

    feat: initialize data producer module

---
+++ src/Modules/DataProducer/Program.cs
@@ -0,0 +1,58 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(options =>
+{
+    options.DocumentName = "v1";
+    options.Title = "Data Producer API";
+    options.Version = "v1";
+});
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi(settings =>
+    {
+        settings.Path = "/openapi/v1.json";
+    });
+    app.MapScalarApiReference(options =>
+    {
+        options.EndpointPathPrefix = "/docs";
+        options.DarkMode = true;
+        options.ShowSideBar = true;
+    });
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
