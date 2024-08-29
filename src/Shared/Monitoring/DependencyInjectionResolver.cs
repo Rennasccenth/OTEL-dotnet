2cddd008e7b36b1d68c795ccbae38ae4b0dbee76
Felipe Nunes <nullnes.sh@gmail.com>
2024-08-29 05:59:51 -0300

    feat: add Open Telemetry default configuration

---
+++ src/Shared/Monitoring/DependencyInjectionResolver.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace Monitoring;
+
+public static class DependencyInjectionResolver
+{
+    public static IHostApplicationBuilder ConfigureOpenTelemetry(this IHostApplicationBuilder hostApplicationBuilder)
+    {
+        hostApplicationBuilder.Logging.AddOpenTelemetry(openTelemetryLoggerOptions =>
+        {
+            openTelemetryLoggerOptions.IncludeFormattedMessage = true;
+            openTelemetryLoggerOptions.IncludeScopes = true;
+        });
+
+        hostApplicationBuilder.Services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(hostApplicationBuilder.Environment.ApplicationName))
+            .WithMetrics(meterProviderBuilder =>
+            {
+                meterProviderBuilder
+                    .AddRuntimeInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation();
+            })
+            .WithTracing(tracerProviderBuilder =>
+            {
+                if (hostApplicationBuilder.Environment.IsDevelopment())
+                {
+                    tracerProviderBuilder.SetSampler<AlwaysOnSampler>();
+                }
+
+                tracerProviderBuilder
+                    .AddGrpcClientInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation();
+            })
+            .WithLogging()
+            .UseOtlpExporter();
+
+        return hostApplicationBuilder;
+    }
+}
