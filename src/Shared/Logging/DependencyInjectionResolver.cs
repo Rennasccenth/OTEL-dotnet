29ac1fe1e6a025cb4ab27cd381e47aea0dbaf419
Felipe Nunes <nullnes.sh@gmail.com>
2024-08-29 05:59:29 -0300

    feat: add serilog default configuration

---
+++ src/Shared/Logging/DependencyInjectionResolver.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Exceptions;
+
+namespace Logging;
+
+public static class DependencyInjectionResolver
+{
+    public static IHostApplicationBuilder ConfigureSerilog(this IHostApplicationBuilder hostApplicationBuilder)
+    {
+        hostApplicationBuilder.Logging.ClearProviders();
+
+        hostApplicationBuilder.Services.AddSerilog(loggerConfiguration =>
+            {
+                loggerConfiguration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithExceptionDetails()
+                    .Enrich.WithCorrelationId()
+                    .WriteTo.Async(writer => writer.Console());
+            },
+            preserveStaticLogger: true,
+            writeToProviders: true);
+
+        return hostApplicationBuilder;
+    } 
+}
