f7832a1f66548a77124c452643e6834a401879b8
Felipe Nunes <nullnes.sh@gmail.com>
2024-09-04 20:41:27 -0300

    feat: add options module

---
+++ src/Shared/Options/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Options;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection RegisterOptionsOf<T>(
+        this IServiceCollection services,
+        string sectionName,
+        bool validateOnStart = true) where T : class
+    {
+        OptionsBuilder<T> optionsBuilder = services.AddOptions<T>()
+            .BindConfiguration(sectionName);
+
+        if (validateOnStart)
+        {
+            optionsBuilder.ValidateDataAnnotations()
+                .ValidateOnStart();
+        }
+            
+        return services;
+    }
+
+    public static IServiceCollection RegisterOptionsOf<T>(this IServiceCollection services, bool validateOnStart = true) where T : class 
+        => services.RegisterOptionsOf<T>(sectionName: typeof(T).Name, validateOnStart);
+}
