cf92c7f53d64d57d00897e96e43f380addee6c71
Felipe Nunes <nullnes.sh@gmail.com>
2024-09-12 06:04:56 -0300

    feat: add otel in Stock.API.csproj

--- src/Modules/Stock.API/Program.cs
+++ src/Modules/Stock.API/Program.cs
@@ -1,21 +1,28 @@
 using System.Reflection;
+using FluentValidation;
 using Logging;
 using Mediation;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Monitoring;
 using Stock.API;
+using Stock.API.ExceptionHandlers;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder
-    .ConfigureSerilog();
+    .ConfigureSerilog()
+    .ConfigureOpenTelemetry();
 
 builder.Services.AddMediation(mediationConfiguration =>
 {
     mediationConfiguration
         .EnableValidation()
-        .UsingAssembly(Assembly.GetExecutingAssembly());
+        .RequireRequestValidator()
+        .UsingAssemblies(Assembly.GetExecutingAssembly());
 });
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 WebApplication app = builder.Build();
 
@@ -26,28 +33,6 @@ WebApplication app = builder.Build();
 //     app.UseSwaggerUI();
 // }
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", (
-        [FromServices] ILogger<Program> logger) =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        
-        logger.LogInformation("Weather forecast: {@Forecast}", forecast);
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
 
 app.MapGet("/weatherforecast2", async (
         [FromServices] IMediator mediator) =>
@@ -70,7 +55,15 @@ namespace Stock.API
     public class GetWeatherForecastRequest : IRequest<WeatherForecast[]>
     {
     }
-    
+
+    internal sealed class GetWeatherForecastRequestValidator : AbstractValidator<GetWeatherForecastRequest>
+    {
+        internal GetWeatherForecastRequestValidator()
+        {
+            
+        }
+    }
+
     internal sealed class GetWeatherForecastRequestHandler : IRequestHandler<GetWeatherForecastRequest, WeatherForecast[]>
     {
         public async Task<WeatherForecast[]> Handle(GetWeatherForecastRequest request, CancellationToken cancellationToken)
